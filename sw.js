f9a289126b2651607dffc9786c26345e83e02046
MinerHUB-DEV <cayden.shinholt@riverwalkacademy.com>
2026-04-29 09:03:24 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 /* sw.js */
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@latest/dist/uv.sw.js');
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.6/dist/uv.bundle.js');
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.6/dist/uv.config.js');
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.6/dist/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
